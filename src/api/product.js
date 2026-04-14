14900fb787adaa727892debf53a59b1dcef520b6
JuHeonParkk <ju10913@naver.com>
2026-04-15 00:32:05 +0900

    feat: product api 분리 작업

---
+++ src/api/product.js
@@ -0,0 +1,74 @@
+import instance from "./instance";
+
+// 상품 조회
+export const getProducts = async () => {
+  const response = await instance.get("/products", {
+    params: {
+      page: 1,
+      pageSize: 10,
+      orderBy: "recent",
+      keyword: "",
+    },
+  });
+  return response.data;
+};
+
+// 상품 등록
+export const createProduct = async (productData) => {
+  const response = await instance.post("/products", productData);
+  return response.data;
+};
+
+// 개별 상품 조회
+export const getProductById = async (productId) => {
+  const response = await instance.get(`/products/${productId}`, {
+    params: {
+      productId,
+    },
+  });
+  return response.data;
+};
+
+// 상품 수정
+export const updateProduct = async (productId, updatedData) => {
+  const response = await instance.patch(
+    `/products/${productId}`,
+    {
+      params: {
+        productId,
+      },
+    },
+    updatedData,
+  );
+  return response.data;
+};
+
+// 상품 삭제
+export const deleteProduct = async (productId) => {
+  const response = await instance.delete(`products/${productId}`, {
+    params: {
+      productId,
+    },
+  });
+  return response.data;
+};
+
+// 상품 좋아요 등록
+export const addLikeProduct = async (productId) => {
+  const response = await instance.post(`products/${productId}/favorite`, {
+    params: {
+      productId,
+    },
+  });
+  return response.data;
+};
+
+// 상품 좋아요 취소
+export const deleteLikeProduct = async (productId) => {
+  const response = await instance.delete(`products/${productId}/favorite`, {
+    params: {
+      productId,
+    },
+  });
+  return response.data;
+};
