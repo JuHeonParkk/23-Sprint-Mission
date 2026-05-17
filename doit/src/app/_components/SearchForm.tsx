fc7d7d0ccd89f5b24045b8c143d23cd1e7c677c1
JuHeonParkk <ju10913@naver.com>
2026-05-18 00:45:41 +0900

    feat: createItem api 연결

--- doit/src/app/_components/SearchForm.tsx
+++ doit/src/app/_components/SearchForm.tsx
@@ -1,18 +1,52 @@
+"use client";
+
+import { FormEvent, useState } from "react";
 import Button from "@/components/common/Button";
 import SearchInput from "@/components/common/SearchInput";
 import PlusIcon from "@/app/assets/icons/PlusIcon";
+import { createItem } from "@/lib/api/item";
 
 export default function SearchForm() {
+  const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value.trim() || isLoading) return;
+
+    try {
+      setIsLoading(true);
+
+      await createItem({ name: value });
+      setValue("");
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="flex items-center justify-between gap-4 pb-10">
-      <SearchInput className="flex-1" placeholder="할 일을 입력해주세요" />
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center justify-between gap-4 pb-10"
+    >
+      <SearchInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="flex-1"
+        placeholder="할 일을 입력해주세요"
+      />
       <Button
         variant="rect"
         style={{ backgroundColor: "var(--color-slate-200)" }}
+        disabled={isLoading}
       >
         <PlusIcon size="16" color="var(--color-slate-900)" />
         <span>추가하기</span>
       </Button>
-    </div>
+    </form>
   );
 }
