d7019c8273d2f3456616cd24a41998c2a6f65f00
JuHeonParkk <ju10913@naver.com>
2026-06-18 18:01:33 +0900

    refactor: button 컴포넌트 상수 바깥 선언

--- doit/src/components/common/Button.tsx
+++ doit/src/components/common/Button.tsx
@@ -6,6 +6,16 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   textColor?: string;
 }
 
+const baseStyle =
+  "flex items-center justify-center gap-2 transition-all  border-[var(--color-slate-900)] border-2 rounded-full text-16-bold active:translate-x-[4px] active:translate-y-[4px] active:shadow-none";
+
+const variants = {
+  rect: "px-8 py-4",
+  circle: "w-14 h-14",
+};
+
+const buttonShadow = "shadow-[6px_6px_var(--color-slate-900)]";
+
 export default function Button({
   variant = "rect",
   color,
@@ -13,19 +23,9 @@ export default function Button({
   children,
   ...props
 }: ButtonProps) {
-  const baseStyle =
-    "flex items-center justify-center gap-2 transition-all  border-[var(--color-slate-900)] border-2 rounded-full text-16-bold active:translate-x-[4px] active:translate-y-[4px] active:shadow-none";
-
-  const variants = {
-    rect: "px-8 py-4",
-    circle: "w-14 h-14",
-  };
-
-  const buttonShadow = "shadow-[6px_6px_var(--color-slate-900)]";
-
   return (
     <button
-      className={`${baseStyle} ${variants[variant]} ${buttonShadow} className`}
+      className={`${baseStyle} ${variants[variant]} ${buttonShadow}`}
       style={{
         backgroundColor: color,
         color: textColor,
